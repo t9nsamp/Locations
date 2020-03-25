50076d79cc0bdbd6dac7cc2b6d03aa80b4a44715
t9nsamp <62615750+t9nsamp@users.noreply.github.com>
2020-03-26 02:17:42 +0700

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ function handleLocationEvent(event) {
   return new Promise((resolve, reject) => {
     restClient.get(`${process.env.apiUrl}?lat=${event.message.latitude}&long=${event.message.longitude}`, (data, response) => {
       if (data) {
-        var pinData = data.map();
+        var pinData = data();
         //   "thumbnailImageUrl": "https://maps.gstatic.com/mapfiles/place_api/icons/bank_dollar-71.png",
         //   "imageBackgroundColor": "#FFFFFF",
         //   "title": `PM 2.5: ${row.results}`,
