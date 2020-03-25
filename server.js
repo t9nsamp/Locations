ada64a6a408c76540a42282e24b3d9d1a391ef66
t9nsamp <62615750+t9nsamp@users.noreply.github.com>
2020-03-25 23:41:20 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ function handleEvent(event) {
 
 function handleLocationEvent(event) {
   return new Promise((resolve, reject) => {
-    restClient.get(`${process.env.apiUrl}?location=${event.message.lat},${event.message.lng}&rankby=distance&name=UOB@&key=AIzaSyAagc52SCi1ns7CggOovTSBMTd8YTXRlRU0`, (data, response) => {
+    restClient.get(`${process.env.apiUrl}?lat=${event.message.latitude}&long=${event.message.longitude}`, (data, response) => {
       if (data) {
         // const pinData = data.map(row => ({
           // "thumbnailImageUrl": row.result[icon],
