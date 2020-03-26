91aaac41f0fc037a4d4aa3509332a383de59eddc
t9nsamp <62615750+t9nsamp@users.noreply.github.com>
2020-03-26 17:15:19 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,303 +1,64 @@
-  const express = require('express')
-  const line = require('@line/bot-sdk')
-  const restClient = new (require('node-rest-client').Client)
+const express = require('express')
+const line = require('@line/bot-sdk')
+const restClient = new (require('node-rest-client').Client)
 
 
-  require('dotenv').config()
-  const app = express()
+require('dotenv').config()
+const app = express()
 
-  const config = {
-    channelAccessToken: process.env.channelAccessToken,
-    channelSecret: process.env.channelSecret
-  };
+const config = {
+  channelAccessToken: process.env.channelAccessToken,
+  channelSecret: process.env.channelSecret
+};
 
-  const client = new line.Client(config);
+const client = new line.Client(config);
 
-  app.get('/', function (req, res) {
-    res.send('03-pm2.5-bot')
-  })
+app.get('/', function (req, res) {
+	res.send('03-pm2.5-bot')
+})
 
-  app.post('/webhook', line.middleware(config), (req, res) => {
-    Promise
-      .all(req.body.events.map(handleEvent))
-      .then((result) => res.json(result))
-      .catch(err => console.log('err', err))
-  });
+app.post('/webhook', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch(err => console.log('err', err))
+});
 
-  function handleEvent(event) {
-    if(event.type === 'message' && event.message.type === 'location') {
-      return handleLocationEvent(event)
-    }else {
-      return Promise.resolve(null)
-    }
+function handleEvent(event) {
+  if(event.type === 'message' && event.message.type === 'location') {
+    return handleLocationEvent(event)
+  }else {
+    return Promise.resolve(null)
   }
+}
 
-  function handleLocationEvent(event) {
-    return new Promise((resolve, reject) => {
+function handleLocationEvent(event) {
+  return new Promise((resolve, reject) => {
 
-      //https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=13.8575872,100.5617152&rankby=distance&keyword=pea&key=AIzaSyAagc52SCi1ns7CggOovTSBMTd8YTXRlRU
+    //https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=13.8575872,100.5617152&rankby=distance&keyword=pea&key=AIzaSyAagc52SCi1ns7CggOovTSBMTd8YTXRlRU
 
-      //apiUrl : https://maps.googleapis.com/maps/api/place/nearbysearch/json
+    //apiUrl : https://maps.googleapis.com/maps/api/place/nearbysearch/json
 
-      restClient.get(`${process.env.apiUrl}?location=${event.message.latitude},${event.message.longitude}&rankby=distance&keyword=UOB&key=AIzaSyAagc52SCi1ns7CggOovTSBMTd8YTXRlRU`, (data, response) => {
+    restClient.get(`${process.env.apiUrl}?location=${event.message.latitude},${event.message.longitude}&rankby=distance&keyword=UOB&key=AIzaSyAagc52SCi1ns7CggOovTSBMTd8YTXRlRU`, (data, response) => {
 
-      //restClient.get(`${process.env.apiUrl}?lat=${event.message.latitude}&long=${event.message.longitude}`, (data, response) => {
+    //restClient.get(`${process.env.apiUrl}?lat=${event.message.latitude}&long=${event.message.longitude}`, (data, response) => {
         if (data) {
           const pinData = data.results.map(row => ({
-            "type": "flex",
-            "altText": "Flex Message",
-            "contents": {
-              "type": "carousel",
-              "contents": [
-                {
-                  "type": "bubble",
-                  "hero": {
-                    "type": "image",
-                    "url": `${row[0].icon}`,
-                    "size": "full",
-                    "aspectRatio": "20:13",
-                    "aspectMode": "cover"
-                  },
-                  "body": {
-                    "type": "box",
-                    "layout": "vertical",
-                    "spacing": "sm",
-                    "contents": [
-                      {
-                        "type": "text",
-                        "text": `PM 2.5 : ${row[0].name}`,
-                        "size": "xl",
-                        "weight": "bold",
-                        "wrap": true
-                      },
-                      {
-                        "type": "box",
-                        "layout": "baseline",
-                        "contents": [
-                          {
-                            "type": "text",
-                            "text": "จุดตรวจวัด ",
-                            
-                            "size": "sm",
-                            "weight": "bold",
-                            "wrap": true
-                          }
-                        ]
-                      }
-                    ]
-                  },
-                  "footer": {
-                    "type": "box",
-                    "layout": "vertical",
-                    "spacing": "sm",
-                    "contents": [
-                      {
-                        "type": "text",
-                        "text": "text"
-                      }
-                    ]
-                  }
-                },
-                {
-                  "type": "bubble",
-                  "hero": {
-                    "type": "image",
-                    "url": `${row[1].icon}`,
-                    "size": "full",
-                    "aspectRatio": "20:13",
-                    "aspectMode": "cover"
-                  },
-                  "body": {
-                    "type": "box",
-                    "layout": "vertical",
-                    "spacing": "sm",
-                    "contents": [
-                      {
-                        "type": "text",
-                        "text": `PM 2.5 : ${row[1].name}`,
-                        "size": "xl",
-                        "weight": "bold",
-                        "wrap": true
-                      },
-                      {
-                        "type": "box",
-                        "layout": "baseline",
-                        "contents": [
-                          {
-                            "type": "text",
-                            "text": "text",
-                            
-                            "size": "sm",
-                            "weight": "bold",
-                            "wrap": true
-                          }
-                        ]
-                      }
-                    ]
-                  },
-                  "footer": {
-                    "type": "box",
-                    "layout": "vertical",
-                    "spacing": "sm",
-                    "contents": [
-                      {
-                        "type": "text",
-                        "text": "อัพเดตเมื่อ"
-                      }
-                    ]
-                  }
-                },
-                {
-                  "type": "bubble",
-                  "hero": {
-                    "type": "image",
-                    "url": `${row[2].icon}`,
-                    "size": "full",
-                    "aspectRatio": "20:13",
-                    "aspectMode": "cover"
-                  },
-                  "body": {
-                    "type": "box",
-                    "layout": "vertical",
-                    "spacing": "sm",
-                    "contents": [
-                      {
-                        "type": "text",
-                        "text": `PM 2.5 : ${row[2].name}`,
-                        "size": "xl",
-                        "weight": "bold",
-                        "wrap": true
-                      },
-                      {
-                        "type": "box",
-                        "layout": "baseline",
-                        "contents": [
-                          {
-                            "type": "text",
-                            "text": "text",
-
-                            "size": "sm",
-                            "weight": "bold",
-                            "wrap": true
-                          }
-                        ]
-                      }
-                    ]
-                  },
-                  "footer": {
-                    "type": "box",
-                    "layout": "vertical",
-                    "spacing": "sm",
-                    "contents": [
-                      {
-                        "type": "text",
-                        "text": "อัพเดตเมื่อ"
-                      }
-                    ]
-                  }
-                }
-        
-              ]
-            }
+            "type": "text",
+            "text": `${row.name}`
           }))
-        
-
-
-
-
-          //   "type": "flex",
-          //   "altText": "Flex Message",
-          //   "contents": {
-          //       "type": "carousel",
-          //       "contents": [
-          //         {
-          //           "type": "bubble",
-          //           "hero": {
-          //             "type": "image",
-          //             "url": "https://f.ptcdn.info/289/063/000/ppdkjp4tguIvW8qTx4iU-o.jpg",
-          //             "size": "full",
-          //             "aspectRatio": "20:13",
-          //             "aspectMode": "cover"
-          //           },
-          //           "body": {
-          //             "type": "box",
-          //             "layout": "vertical",
-          //             "spacing": "sm",
-          //             "contents": [
-          //               {
-          //                 "type": "text",
-          //                 "text": `${row[0].name}`,
-          //                 "size": "xl",
-          //                 "weight": "bold",
-          //                 "wrap": true
-          //               },
-          //               {
-          //                 "type": "box",
-          //                 "layout": "baseline",
-          //                 "contents": [
-          //                   {
-          //                     "type": "text",
-          //                     "text": "$49",
-                              
-          //                     "size": "xl",
-          //                     "weight": "bold",
-          //                     "wrap": true
-          //                   },
-          //                   {
-          //                     "type": "text",
-          //                     "text": ".99",
-                            
-          //                     "size": "sm",
-          //                     "weight": "bold",
-          //                     "wrap": true
-          //                   }
-          //                 ]
-          //               }
-          //             ]
-          //           },
-          //           "footer": {
-          //             "type": "box",
-          //             "layout": "vertical",
-          //             "spacing": "sm",
-          //             "contents": [
-          //               {
-          //                 "type": "button",
-          //                 "action": {
-          //                   "type": "uri",
-          //                   "label": "นำทาง",
-          //                   "uri": `https://www.google.com/maps/dir/${event.message.latitude},${event.message.longitude}/${row[0].geometry.location.lat},${row[0].geometry.location.lng}`
-          //                 },
-          //                 "style": "primary"
-          //               },
-          //               {
-          //                 "type": "button",
-          //                 "action": {
-          //                   "type": "uri",
-          //                   "label": "Add to whishlist",
-          //                   "uri": "https://linecorp.com"
-          //                 }
-          //               }
-          //             ]
-          //           }
-          //         }
-          //       ]
-          //   }
-            
-          // }))
-        
-    
-            resolve(client.replyMessage(event.replyToken, msg))
-          } else {
-            reject()
-          }
-        })
+  
+          resolve(client.replyMessage(event.replyToken, msg))
+        } else {
+          reject()
+        }
       })
-    
-    }
-    
-    app.set('port', (process.env.PORT || 4000))
-    
-    app.listen(app.get('port'), function () {
-      console.log('run at port', app.get('port'))
     })
-        
+   
+  }
+  
+  app.set('port', (process.env.PORT || 4000))
+  
+  app.listen(app.get('port'), function () {
+    console.log('run at port', app.get('port'))
+  })
