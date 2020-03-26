b66a38e9197f0b7b1377ee42c24e19ed96586dc4
t9nsamp <62615750+t9nsamp@users.noreply.github.com>
2020-03-26 23:40:17 +0700

    Update server.js

--- server.js
+++ server.js
@@ -39,47 +39,95 @@
 
       //apiUrl : https://maps.googleapis.com/maps/api/place/nearbysearch/json
 
-      //restClient.get(`${process.env.apiUrl}?location=${event.message.latitude},${event.message.longitude}&rankby=distance&keyword=UOB@&key=AIzaSyAagc52SCi1ns7CggOovTSBMTd8YTXRlRU`, (data, response) => {
+      restClient.get(`${process.env.apiUrl}?location=${event.message.latitude},${event.message.longitude}&rankby=distance&keyword=UOB@&key=AIzaSyAagc52SCi1ns7CggOovTSBMTd8YTXRlRU`, (data, response) => {
 
-      restClient.get(`${process.env.apiUrl}?lat=${event.message.latitude}&long=${event.message.longitude}`, (data, response) => {
+      //restClient.get(`${process.env.apiUrl}?lat=${event.message.latitude}&long=${event.message.longitude}`, (data, response) => {
           if (data) {
-            //const pinData = data.results.map(row => ({
-              const pinData = data.map(row => ({
-                "thumbnailImageUrl": "https://f.ptcdn.info/289/063/000/ppdkjp4tguIvW8qTx4iU-o.jpg",
-                "imageBackgroundColor": "#FFFFFF",
-                "title": `${row.aqi.aqi}`,
-                "text": `${row.aqi.aqi}`,
-                "actions": [
-                  {
-                    "type": "uri",
-                    "label": "ข้อมูลย้อนหลัง",
-                    "uri": "https://www.uob.co.th/default/index.page"
-                  }
-                ]
-              }))
-          
-              var msg = {
-                "type": "template",
-                "altText": "ข้อมูลสถานที่",
-                "template": {
+            const pinData = data.results.map(row => ({
+              //const pinData = data.map(row => ({
+                "type": "flex",
+                "altText": "Flex Message",
+                "contents": {
                   "type": "carousel",
-                  "columns": (pinData[0],pinData[1],pinData[2]),
-                  "imageAspectRatio": "rectangle",
-                  "imageSize": "cover"
+                  "contents": [
+                    {
+                      "type": "bubble",
+                      "hero": {
+                        "type": "image",
+                        "url": "https://f.ptcdn.info/289/063/000/ppdkjp4tguIvW8qTx4iU-o.jpg",
+                        "size": "full",
+                        "aspectRatio": "20:13",
+                        "aspectMode": "cover"
+                      },
+                      "body": {
+                        "type": "box",
+                        "layout": "vertical",
+                        "spacing": "sm",
+                        "contents": [
+                          {
+                            "type": "text",
+                            "text": `${row.name}`,
+                            "size": "xl",
+                            "weight": "bold",
+                            "wrap": true
+                          },
+                          {
+                            "type": "box",
+                            "layout": "baseline",
+                            "contents": [
+                              {
+                                "type": "text",
+                                "text": `${row.vicinity}`,
+                                "size": "sm",
+                                "weight": "bold",
+                                "wrap": true
+                              }
+  
+                            ]
+                          }
+                        ]
+                      },
+                      "footer": {
+                        "type": "box",
+                        "layout": "vertical",
+                        "spacing": "sm",
+                        "contents": [
+                          {
+                            "type": "button",
+                            "action": {
+                              "type": "uri",
+                              "label": "นำทาง",
+                              "uri":  `https://www.google.com/maps/dir/${event.message.latitude},${event.message.longitude}/${row.geometry.location.lat},${row.geometry.location.lng}`
+                            },
+                            "style": "primary"
+                          },
+                          {
+                            "type": "button",
+                            "action": {
+                              "type": "uri",
+                              "label": "เว็บไซต์",
+                              "uri": "https://www.uob.co.th/default/index.page"
+                            }
+                        
+                          }
+                        ]
+                      }
+                    }
+                  ]
                 }
-              }
-      
-              resolve(client.replyMessage(event.replyToken, msg))
-            } else {
-              reject()
-            }
-          })
+              
+            }))
+            var msg = [];
+  
+            msg.push(pinData[0],pinData[1],pinData[2])
+    
+            resolve(client.replyMessage(event.replyToken, msg))
+          } else {
+            reject()
+          }
         })
-      
-      }
-      
-      app.set('port', (process.env.PORT || 4000))
-      
-      app.listen(app.get('port'), function () {
-        console.log('run at port', app.get('port'))
       })
+     
+    }
+    
+  
