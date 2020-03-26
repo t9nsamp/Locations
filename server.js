fd489f262b7bae0216a67eff6e3bb3160fe0fb75
t9nsamp <62615750+t9nsamp@users.noreply.github.com>
2020-03-26 13:22:24 +0700

    Update server.js

--- server.js
+++ server.js
@@ -39,15 +39,95 @@ function handleLocationEvent(event) {
 
     //apiUrl : https://maps.googleapis.com/maps/api/place/nearbysearch/json
 
-    restClient.get(`${process.env.apiUrl}?location=${event.message.latitude},${event.message.longitude}&rankby=distance&keyword=pea&key=AIzaSyAagc52SCi1ns7CggOovTSBMTd8YTXRlRU`, (data, response) => {
+    restClient.get(`${process.env.apiUrl}?location=${event.message.latitude},${event.message.longitude}&rankby=distance&keyword=ธนาคารUOB&key=AIzaSyAagc52SCi1ns7CggOovTSBMTd8YTXRlRU`, (data, response) => {
 
     //restClient.get(`${process.env.apiUrl}?lat=${event.message.latitude}&long=${event.message.longitude}`, (data, response) => {
         if (data) {
             //const pinData = data.results
           const pinData = data.results.map(row => ({
-            "type": "text",
-            "text": `${row.name}`
-          }))
+              "type": "flex",
+              "altText": "Flex Message",
+              "contents": {
+                "type": "bubble",
+                "header": {
+                  "type": "box",
+                  "layout": "horizontal",
+                  "contents": [
+                    {
+                      "type": "text",
+                      "text": "ธนาคาร UOB " + `${row.name}`,
+                      "size": "sm",
+                      "weight": "bold",
+                      "color": "#AAAAAA"
+                    }
+                  ]
+                },
+                "hero": {
+                  "type": "image",
+                 "url": "https://lh3.googleusercontent.com/p/AF1QipP4xdrNQYSC-T4aXbBfvUujlyS98Dbz1109JtBL=w1024-k",
+                  "size": "full",
+                  "aspectRatio": "20:13",
+                  "aspectMode": "cover",
+                  "action": {
+                    "type": "uri",
+                    "label": "Action",
+                    "uri": "https://linecorp.com/"
+                  }
+                },
+                "body": {
+                  "type": "box",
+                  "layout": "horizontal",
+                  "spacing": "md",
+                  "contents": [
+                    {
+                      "type": "box",
+                      "layout": "vertical",
+                      "contents": [
+                        {
+                          "type": "text",
+                          "text": "ที่อยู่ " + `${row.name}`,
+                          "margin": "md",
+                          "size": "xs",
+                          "gravity": "top"
+                        },
+                        {
+                          "type": "separator"
+                        },
+                        {
+                          "type": "text",
+                          "text": "เบอร์ติดต่อ " + `${row.name}`,
+                          "margin": "md",
+                          "size": "xs",
+                          "gravity": "center"
+                        }
+                     
+                      ]
+                    }
+                  ]
+                },
+                "footer": {
+                  "type": "box",
+                  "layout": "horizontal",
+                  "contents": [
+                    {
+                      "type": "button",
+                      "action": {
+                        "type": "uri",
+                        "label": "นำทาง",
+                        "uri": "https://www.google.com/maps?q="
+                      }
+                    }
+                  ]
+                }
+              }
+            }
+            
+             //end fulfillment
+             //end result
+
+            // "type": "text",
+            // "text": `${row.name}`
+))
 
           var msg = [];
 
