debe5c83959835f5c6a9a4771d3777cfdace56f4
t9nsamp <62615750+t9nsamp@users.noreply.github.com>
2020-03-26 22:02:31 +0700

    Update server.js

--- server.js
+++ server.js
@@ -39,21 +39,21 @@ function handleLocationEvent(event) {
 
     //apiUrl : https://maps.googleapis.com/maps/api/place/nearbysearch/json
 
-    //restClient.get(`${process.env.apiUrl}?location=${event.message.latitude},${event.message.longitude}&rankby=distance&keyword=UOB&key=AIzaSyAagc52SCi1ns7CggOovTSBMTd8YTXRlRU`, (data, response) => {
+    restClient.get(`${process.env.apiUrl}?location=${event.message.latitude},${event.message.longitude}&rankby=distance&keyword=UOB&key=AIzaSyAagc52SCi1ns7CggOovTSBMTd8YTXRlRU`, (data, response) => {
 
-    restClient.get(`${process.env.apiUrl}?lat=${event.message.latitude}&long=${event.message.longitude}`, (data, response) => {
+    //restClient.get(`${process.env.apiUrl}?lat=${event.message.latitude}&long=${event.message.longitude}`, (data, response) => {
         if (data) {
-          //const pinData = data.results.map(row => ({
-            const pinData = data.map(row => ({
+          const pinData = data.results.map(row => ({
+            //const pinData = data.map(row => ({
               "thumbnailImageUrl": "https://f.ptcdn.info/289/063/000/ppdkjp4tguIvW8qTx4iU-o.jpg",
               "imageBackgroundColor": "#FFFFFF",
-              "title": `PM 2.5: ${row.aqi.aqi}`,
+              "title": `PM 2.5: ${row.name}`,
               "text": "ข้อมูลย้อนหลัง",
               "actions": [
                 {
                   "type": "uri",
                   "label": "ข้อมูลย้อนหลัง",
-                  "uri": row.historyUrl
+                  "uri": "https://www.uob.co.th/default/index.page"
                 }
               ]
             }))
