481734b7e4b2f0bde8de9c2d4cbb9eb5f09091aa
t9nsamp <62615750+t9nsamp@users.noreply.github.com>
2020-03-28 01:34:02 +0700

    Update server.js

--- server.js
+++ server.js
@@ -43,7 +43,7 @@
 
           //restClient.get(`${process.env.apiUrl}?lat=${event.message.latitude}&long=${event.message.longitude}`, (data, response) => {
               if (data) {
-                const pinData = data.results.map(row => ({
+                 data.results.map(row => ({
                   //const pinData = data.map(row => ({
                     "type": "flex",
                     "altText": "Flex Message",
