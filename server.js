cf4558e2f9d1ec839fb5c5758f16db94f80073bd
t9nsamp <62615750+t9nsamp@users.noreply.github.com>
2020-03-27 13:33:29 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,140 +1,140 @@
-    const express = require('express')
-    const line = require('@line/bot-sdk')
-    const restClient = new (require('node-rest-client').Client)
+      const express = require('express')
+      const line = require('@line/bot-sdk')
+      const restClient = new (require('node-rest-client').Client)
 
 
-    require('dotenv').config()
-    const app = express()
+      require('dotenv').config()
+      const app = express()
 
-    const config = {
-      channelAccessToken: process.env.channelAccessToken,
-      channelSecret: process.env.channelSecret
-    };
+      const config = {
+        channelAccessToken: process.env.channelAccessToken,
+        channelSecret: process.env.channelSecret
+      };
 
-    const client = new line.Client(config);
+      const client = new line.Client(config);
 
-    app.get('/', function (req, res) {
-      res.send('03-pm2.5-bot')
-    })
-
-    app.post('/webhook', line.middleware(config), (req, res) => {
-      Promise
-        .all(req.body.events.map(handleEvent))
-        .then((result) => res.json(result))
-        .catch(err => console.log('err', err))
-    });
-
-    function handleEvent(event) {
-      if(event.type === 'message' && event.message.type === 'location') {
-        return handleLocationEvent(event)
-      }else {
-        return Promise.resolve(null)
-      }
-    }
-
-    function handleLocationEvent(event) {
-      return new Promise((resolve, reject) => {
-
-        //https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=13.8575872,100.5617152&rankby=distance&keyword=pea&key=AIzaSyAagc52SCi1ns7CggOovTSBMTd8YTXRlRU
-
-        //apiUrl : https://maps.googleapis.com/maps/api/place/nearbysearch/json
-
-        restClient.get(`${process.env.apiUrl}?location=${event.message.latitude},${event.message.longitude}&rankby=distance&keyword=UOB@&key=AIzaSyAagc52SCi1ns7CggOovTSBMTd8YTXRlRU`, (data, response) => {
-
-        //restClient.get(`${process.env.apiUrl}?lat=${event.message.latitude}&long=${event.message.longitude}`, (data, response) => {
-            if (data) {
-              const pinData = data.results.map(row => ({
-                //const pinData = data.map(row => ({
-                  "type": "flex",
-                  "altText": "Flex Message",
-                  "contents": {
-                    "type": "carousel",
-                    "contents": [
-                      {
-                        "type": "bubble",
-                        "hero": {
-                          "type": "image",
-                          "url": "https://f.ptcdn.info/289/063/000/ppdkjp4tguIvW8qTx4iU-o.jpg",
-                          "size": "full",
-                          "aspectRatio": "20:13",
-                          "aspectMode": "cover"
-                        },
-                        "body": {
-                          "type": "box",
-                          "layout": "vertical",
-                          "spacing": "sm",
-                          "contents": [
-                            {
-                              "type": "text",
-                              "text": `${row.name}`,
-                              "size": "xl",
-                              "weight": "bold",
-                              "wrap": true
-                            },
-                            {
-                              "type": "box",
-                              "layout": "baseline",
-                              "contents": [
-                                {
-                                  "type": "text",
-                                  "text": `${row.vicinity}`,
-                                  "size": "sm",
-                                  "weight": "bold",
-                                  "wrap": true
-                                }
-    
-                              ]
-                            }
-                          ]
-                        },
-                        "footer": {
-                          "type": "box",
-                          "layout": "vertical",
-                          "spacing": "sm",
-                          "contents": [
-                            {
-                              "type": "button",
-                              "action": {
-                                "type": "uri",
-                                "label": "นำทาง",
-                                "uri":  `https://www.google.com/maps/dir/${event.message.latitude},${event.message.longitude}/${row.geometry.location.lat},${row.geometry.location.lng}`
-                              },
-                              "style": "primary"
-                            },
-                            {
-                              "type": "button",
-                              "action": {
-                                "type": "uri",
-                                "label": "เว็บไซต์",
-                                "uri": "https://www.uob.co.th/default/index.page"
-                              }
-                          
-                            }
-                          ]
-                        }
-                      }
-                    ]
-                  }
-                
-              }))
-              
-              var msg = [];
-    
-              msg.push(pinData[0],pinData[1],pinData[2])
-      
-              resolve(client.replyMessage(event.replyToken, msg))
-            } else {
-              reject()
-            }
-          })
-        })
-      
-      }
-      
-      app.set('port', (process.env.PORT || 4000))
-      
-      app.listen(app.get('port'), function () {
-        console.log('run at port', app.get('port'))
+      app.get('/', function (req, res) {
+        res.send('03-pm2.5-bot')
       })
 
+      app.post('/webhook', line.middleware(config), (req, res) => {
+        Promise
+          .all(req.body.events.map(handleEvent))
+          .then((result) => res.json(result))
+          .catch(err => console.log('err', err))
+      });
+
+      function handleEvent(event) {
+        if(event.type === 'message' && event.message.type === 'location') {
+          return handleLocationEvent(event)
+        }else {
+          return Promise.resolve(null)
+        }
+      }
+
+      function handleLocationEvent(event) {
+        return new Promise((resolve, reject) => {
+
+          //https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=13.8575872,100.5617152&rankby=distance&keyword=pea&key=AIzaSyAagc52SCi1ns7CggOovTSBMTd8YTXRlRU
+
+          //apiUrl : https://maps.googleapis.com/maps/api/place/nearbysearch/json
+
+          restClient.get(`${process.env.apiUrl}?location=${event.message.latitude},${event.message.longitude}&rankby=distance&keyword=UOB@&key=AIzaSyAagc52SCi1ns7CggOovTSBMTd8YTXRlRU`, (data, response) => {
+
+          //restClient.get(`${process.env.apiUrl}?lat=${event.message.latitude}&long=${event.message.longitude}`, (data, response) => {
+              if (data) {
+                const pinData = data.results.map(row => ({
+                  //const pinData = data.map(row => ({
+                    "type": "flex",
+                    "altText": "Flex Message",
+                    "contents": {
+                      "type": "carousel",
+                      "contents": [
+                        {
+                          "type": "bubble",
+                          "hero": {
+                            "type": "image",
+                            "url": "https://f.ptcdn.info/289/063/000/ppdkjp4tguIvW8qTx4iU-o.jpg",
+                            "size": "full",
+                            "aspectRatio": "20:13",
+                            "aspectMode": "cover"
+                          },
+                          "body": {
+                            "type": "box",
+                            "layout": "vertical",
+                            "spacing": "sm",
+                            "contents": [
+                              {
+                                "type": "text",
+                                "text": `${row.name}`,
+                                "size": "xl",
+                                "weight": "bold",
+                                "wrap": true
+                              },
+                              {
+                                "type": "box",
+                                "layout": "baseline",
+                                "contents": [
+                                  {
+                                    "type": "text",
+                                    "text": `${row.opening_hours.open_now}`,
+                                    "size": "sm",
+                                    "weight": "bold",
+                                    "wrap": true
+                                  }
+      
+                                ]
+                              }
+                            ]
+                          },
+                          "footer": {
+                            "type": "box",
+                            "layout": "vertical",
+                            "spacing": "sm",
+                            "contents": [
+                              {
+                                "type": "button",
+                                "action": {
+                                  "type": "uri",
+                                  "label": "นำทาง",
+                                  "uri":  `https://www.google.com/maps/dir/${event.message.latitude},${event.message.longitude}/${row.geometry.location.lat},${row.geometry.location.lng}`
+                                },
+                                "style": "primary"
+                              },
+                              {
+                                "type": "button",
+                                "action": {
+                                  "type": "uri",
+                                  "label": "เว็บไซต์",
+                                  "uri": "https://www.uob.co.th/default/index.page"
+                                }
+                            
+                              }
+                            ]
+                          }
+                        }
+                      ]
+                    }
+                  
+                }))
+                
+                var msg = [];
+      
+                msg.push(pinData[0],pinData[1],pinData[2])
         
+                resolve(client.replyMessage(event.replyToken, msg))
+              } else {
+                reject()
+              }
+            })
+          })
+        
+        }
+        
+        app.set('port', (process.env.PORT || 4000))
+        
+        app.listen(app.get('port'), function () {
+          console.log('run at port', app.get('port'))
+        })
+
+          
