f859698c98ad6fe4995e1d95c4bc3e5f659614b1
t9nsamp <62615750+t9nsamp@users.noreply.github.com>
2020-03-28 18:18:56 +0700

    Update server.js

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ function handleLocationEvent(event) {
                         },
                         {
                           "type": "text",
-                          "text": calculate(),
+                          "text": calculate(event.message.latitude,event.message.longitude,row.geometry.location.lat,row.geometry.location.lng),
                           "wrap": true,
                           "color": "#666666",
                           "size": "sm",
@@ -170,9 +170,27 @@ function handleLocationEvent(event) {
     })
    
   }
- function calculate(){
-   return "dddddd";
- }
+  function calculate(lat1, lon1, lat2, lon2, unit) {
+    if ((lat1 == lat2) && (lon1 == lon2)) {
+      return 0;
+    }
+    else {
+      var radlat1 = Math.PI * lat1/180;
+      var radlat2 = Math.PI * lat2/180;
+      var theta = lon1-lon2;
+      var radtheta = Math.PI * theta/180;
+      var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+      if (dist > 1) {
+        dist = 1;
+      }
+      dist = Math.acos(dist);
+      dist = dist * 180/Math.PI;
+      dist = dist * 60 * 1.1515;
+      if (unit=="K") { dist = dist * 1.609344 }
+      if (unit=="N") { dist = dist * 0.8684 }
+      return dist;
+    }
+  }
   
   app.set('port', (process.env.PORT || 4000))
   
