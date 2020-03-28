07a531678a33d159d8552ea8b286996dc33cade7
t9nsamp <62615750+t9nsamp@users.noreply.github.com>
2020-03-28 17:37:33 +0700

    Update server.js

--- server.js
+++ server.js
@@ -44,88 +44,6 @@ function handleLocationEvent(event) {
       //restClient.get(`${process.env.apiUrl}?lat=${event.message.latitude}&long=${event.message.longitude}`, (data, response) => {
         
       if (data) {
-        var dist_;
-        var bounds = new google.maps.LatLngBounds();
-        var markersArray = [];
-        var origin1 = { lat: event.message.latitude, lng: event.message.longitude };       
-        var destinationB = { lat: data.results.geometry.location.lat, lng: data.results.geometry.location.lng };
-        var destinationIcon =
-          "https://chart.googleapis.com/chart?" +
-          "chst=d_map_pin_letter&chld=D|FF0000|000000";
-        var originIcon =
-          "https://chart.googleapis.com/chart?" +
-          "chst=d_map_pin_letter&chld=O|FFFF00|000000";
-        var map = new google.maps.Map(document.getElementById("map"), {
-          center: { lat: 13.736717, lng: 	100.523186 },
-          zoom: 10
-        });
-        var geocoder = new google.maps.Geocoder();
-
-        var service = new google.maps.DistanceMatrixService();
-        service.getDistanceMatrix(
-          {
-            origins: [origin1],
-            destinations: [destinationB],
-            travelMode: "DRIVING",
-            unitSystem: google.maps.UnitSystem.METRIC,
-            avoidHighways: false, 
-            avoidTolls: false
-          },
-          function(response, status) {
-            if (status !== "OK") {
-              alert("Error was : " + status);
-            } else {
-              var originList = response.originAddresses;
-              var destinationList = response.destinationAddresses;
-              var outputDiv = document.getElementById("output");
-              outputDiv.innerHTML = "";
-              deleteMarkers(markersArray);
-
-              var showGeocodedAddressOnMap = function(asDestination) {
-                var icon = asDestination ? destinationIcon : originIcon;
-                return function(results, status) {
-                  if (status === "OK") {
-                    map.fitBounds(bounds.extend(results[0].geometry.location));
-                    markersArray.push(
-                      new google.maps.Marker({
-                        map: map,
-                        position: results[0].geometry.location,
-                        icon: icon
-                      })
-                    );
-                  } else {
-                    alert("Geocode was not successful due to: " + status);
-                  }
-                };
-              };
-
-              for (var i = 0; i < originList.length; i++) {
-                var results = response.rows[i].elements;
-                geocoder.geocode(
-                  { address: originList[i] },
-                  showGeocodedAddressOnMap(false)
-                );
-                for (var j = 0; j < results.length; j++) {
-                  geocoder.geocode(
-                    { address: destinationList[j] },
-                    showGeocodedAddressOnMap(true)
-                  );
-                  outputDiv.innerHTML +=
-                  results[j].distance.text;
-                   dist_ = results[j].distance.text;
-                }
-              }
-            }
-          }
-        );
-      
-
-      function deleteMarkers(markersArray) {
-        for (var i = 0; i < markersArray.length; i++) {
-          markersArray[i].setMap(null);
-        }
-        markersArray = [];
-      }
           const pinData = data.results.map(row => ({
             "type": "bubble",
             "body": {
@@ -174,7 +92,7 @@ function handleLocationEvent(event) {
                       "contents": [
                         {
                           "type": "text",
-                          "text": dist_,
+                          "text": "เวล่า",
                           "color": "#aaaaaa",
                           "size": "sm",
                           "flex": 1
