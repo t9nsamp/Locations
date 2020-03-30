64d8f743eb69a0d4ed667d887dbd1f74cfcb3e0f
t9nsamp <62615750+t9nsamp@users.noreply.github.com>
2020-03-30 11:40:54 +0700

    Update server.js

--- server.js
+++ server.js
@@ -157,7 +157,7 @@ function handleLocationEvent(event) {
             "type": "flex",
             "altText": "Flex Message",
             "contents": {
-                "type": "bubble",
+                "type": "carousel",
                 "contents": CC
           }
         }
