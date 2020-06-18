9e4855d002edab4647fed911ce95a52c28eb0430
t9nsamp <62615750+t9nsamp@users.noreply.github.com>
2020-06-19 00:16:49 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,62 +1,162 @@
-const express = require('express')
-const line = require('@line/bot-sdk')
-const restClient = new (require('node-rest-client').Client)
+    const express = require('express')
+  const bodyParser = require('body-parser');
+  var request = require('request');
+  const line = require('@line/bot-sdk')
+  const restClient = new (require('node-rest-client').Client)
+  const excelToJson = require('convert-excel-to-json')
+  const geolib = require('geolib')
 
+  const peavolta_bank = excelToJson(
+    {
+      sourceFile: 'Bank.xlsx',
+      columnToKey: {
+        '*': '{{columnHeader}}'
+      } ,
+      range: 'A2:Q156'
+   })
 
-require('dotenv').config()
-const app = express()
+   const peavolta_atm = excelToJson(
+    {
+      sourceFile: 'Atm.xlsx',
+      columnToKey: {
+        '*': '{{columnHeader}}'
+      } ,
+      range: 'A2:Q423'
+   })
 
-const config = {
-  channelAccessToken: process.env.channelAccessToken,
-  channelSecret: process.env.channelSecret
-};
+   const peavolta_pum = excelToJson(
+    {
+      sourceFile: 'Pum.xlsx',
+      columnToKey: {
+        '*': '{{columnHeader}}'
+      } ,
+      range: 'A2:Q38'
+   })
 
-const client = new line.Client(config);
+   const peavolta_tmrw = excelToJson(
+    {
+      sourceFile: 'Tmrw.xlsx',
+      columnToKey: {
+        '*': '{{columnHeader}}'
+      } ,
+      range: 'A2:Q235'
+   })
 
-app.get('/', function (req, res) {
-	res.send('03-pm2.5-bot')
-})
+   var text = "text"
 
-app.post('/webhook', line.middleware(config), (req, res) => {
-  Promise
-    .all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result))
-    .catch(err => console.log('err', err))
-});
+  // const peavolta_atm = excelToJson({
+  //   sourceFile: 'atm.xlsx',
+  //   columnToKey: {
+  //       '*': '{{columnHeader}}'
+  //   },
+  //   range: 'A2:N422'
+  // })
 
-function handleEvent(event) {
-  if(event.type === 'message' && event.message.type === 'location') {
-    return handleLocationEvent(event)
-  }else {
-    return Promise.resolve(null)
+  // const peavolta_pub = excelToJson({
+  //   sourceFile: 'pub.xlsx',
+  //   columnToKey: {
+  //       '*': '{{columnHeader}}'
+  //   },
+  //   range: 'A2:N38'
+  // })
+
+  require('dotenv').config()
+  const app = express() 
+
+  const config = {
+    channelAccessToken: process.env.channelAccessToken,
+    channelSecret: process.env.channelSecret
   }
-}
 
-function handleLocationEvent(event) {
-    
-  return new Promise((resolve, reject) => {
+  app.use(express.static('public'));
 
-    //https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=13.8575872,100.5617152&rankby=distance&keyword=pea&key=AIzaSyAagc52SCi1ns7CggOovTSBMTd8YTXRlRU
+  const client = new line.Client(config)
 
-    //apiUrl : https://maps.googleapis.com/maps/api/place/nearbysearch/json
+  app.get('/', function (req, res) {
+    res.send('UOB-bot')
+  })
 
-    restClient.get(`${process.env.apiUrl}?location=${event.message.latitude},${event.message.longitude}&rankby=distance&keyword=UOB&key=AIzaSyAagc52SCi1ns7CggOovTSBMTd8YTXRlRU`, (data, response) => {
-      //restClient.get(`${process.env.apiUrl}?lat=${event.message.latitude}&long=${event.message.longitude}`, (data, response) => {    
-      if (data) {
-          var x = "77777"
-          const pinData = data.results.map(row => ({
+  app.post('/webhook', line.middleware(config), (req, res) => {
+
+      if(req.body.events[0].type === 'message'  && req.body.events[0].message.type === 'text'){
+
+          postToDialogflow(req);
+         text = req.body.events[0].message.text
+        }
+
+      else if (req.body.events[0].type === 'message' && req.body.events[0].message.type === 'location'){ 
+
+    Promise
+      //.all(req.body.events.map(handleEvent))
+      .all(req.body.events.map(handleLocationEvent))
+      .then((result) => res.json(result))
+      .catch(err => console.log('err', err))
+
+      }
+
+  });
+
+
+  var select = (a) =>{
+    if( a == "bank"){ return peavolta_bank.Sheet1}
+    else if( a == "atm") {return peavolta_atm.Sheet1}
+    else if( a == "pum") {return peavolta_pum.Sheet1}
+    else if( a == "tmrw") {return peavolta_tmrw.Sheet1}
+  }
+
+
+  function handleLocationEvent(event) {
+ //   var sheet_select = ('text1','text2')
+ 
+    return new Promise((resolve, reject) => {
+        var userlat = parseFloat(event.message.latitude)
+        var userlng = parseFloat(event.message.longitude)
+        var voltajson = select(text)       
+        //peavolta.Sheet1
+        // for loop to calculate distance for all station
+        for(var i = 0; i < voltajson.length; i++) {
+          var obj = voltajson[i];
+          var placelat = obj.lat
+          var placelng = obj.lng
+          // get distance 
+          var distacne = geolib.getDistance(
+            { latitude: userlat, longitude: userlng },
+            { latitude: placelat, longitude: placelng }
+          )
+          voltajson[i].distacne = Math.ceil((distacne/1000) * 10) / 10 // write distance to voltajson
+        } // end for loop
+        // sort object in json array by distance
+        voltajson.sort(function(a, b){
+          return a.distacne - b.distacne;
+        })
+        var result = []
+        // collect only first 5 elements
+        for (var i = 0; i < 5; i++) {
+            result.push(voltajson[i])
+        }
+
+        if (result) {
+          const pinData = result.map(row => ({
             "type": "bubble",
+            "size": "kilo",
             "body": {
               "type": "box",
               "layout": "vertical",
               "contents": [
                 {
-                  "type": "text",
-                  "text": `${row.name}`,
-                  "weight": "bold",
-                  "size": "lg",
-                  "wrap": true
-                },
+                  "type": "box",
+                  "layout": "baseline",
+                  "margin": "md",
+                  "contents": [
+                    {
+                      "type": "text",
+                      "text": `${row.category} ${row.name}`,
+                      "weight": "bold",
+                      "size": "lg",
+                      "wrap": true
+                    }
+                  ]
+                },      
                 {
                   "type": "box",
                   "layout": "vertical",
@@ -69,19 +169,37 @@ function handleLocationEvent(event) {
                       "spacing": "sm",
                       "contents": [
                         {
+                          "type": "icon",
+                          "size": "3xl",
+                          "url": row.logo
+                        },
+
+                      ]
+                    },
+                    {
+                      "type": "box",
+                      "layout": "baseline",
+                      "spacing": "sm",
+                      "contents": [
+                        {
+                          "type": "icon",
+                          "url": "https://ran-ln.tk/saveIMG/location.png"
+                        },
+
+                        {
                           "type": "text",
                           "text": "ที่อยู่",
                           "color": "#aaaaaa",
                           "size": "sm",
-                          "flex": 1
+                          "flex": 3
                         },
                         {
                           "type": "text",
-                          "text": `${row.vicinity}`,
+                          "text": `${row.address}`,
                           "wrap": true,
                           "color": "#666666",
                           "size": "sm",
-                          "flex": 5
+                          "flex": 6
                         }
                       ]
                     },
@@ -91,27 +209,107 @@ function handleLocationEvent(event) {
                       "spacing": "sm",
                       "contents": [
                         {
+                          "type": "icon",
+                          "url": "https://ran-ln.tk/saveIMG/time.png"
+                        },
+
+                        {
                           "type": "text",
-                          "text": "ระยะทาง",
+                          "text": "เวลาทำการ",
                           "color": "#aaaaaa",
                           "size": "sm",
-                          "flex": 1
+                          "flex": 3
                         },
                         {
                           "type": "text",
-                          "text":  calculate(`${event.message.latitude}`,`${event.message.longitude}`,`${row.geometry.location.lat}`,`${row.geometry.location.lng}`),
+                          "text": `${row.workday} `,
                           "wrap": true,
                           "color": "#666666",
                           "size": "sm",
-                          "flex": 5
+                          "flex": 6
+                        }
+                      ]
+                    },
+                    {
+                      "type": "box",
+                      "layout": "baseline",
+                      "spacing": "sm",
+                      "contents": [
+                        {
+                          "type": "icon",
+                          "url": "https://ran-ln.tk/saveIMG/phone2.png"
+                        }, 
+
+                        {
+                          "type": "text",
+                          "text": "ติดต่อ",
+                          "color": "#aaaaaa",
+                          "size": "sm",
+                          "flex": 3
+                        },
+                        {
+                          "type": "text",
+                          "text": `${row.tel}`,
+                          "wrap": true,
+                          "color": "#666666",
+                          "size": "sm",
+                          "flex": 6
+                        }
+                      ]
+                    },
+                    {
+                      "type": "box",
+                      "layout": "baseline",
+                      "spacing": "sm",
+                      "contents": [  
+                        {
+                          "type": "icon",
+                          "url": "https://ran-ln.tk/saveIMG/latlng1.png"
+                        }, 
+
+                        {
+                          "type": "text",
+                          "text": "ระยะทาง",
+                          "color": "#aaaaaa", 
+                          "size": "sm",
+                          "flex": 3
+                        },
+                        {
+                          "type": "text",
+                          "text":  `${row.distacne} km`,
+                          "wrap": true,
+                          "color": "#666666",
+                          "size": "sm",
+                          "flex": 6
+                        }
+                      ]
+                    },
+                    {
+                      "type": "box",
+                      "layout": "baseline",
+                      "spacing": "sm",
+                      "contents": [  
+
+
+                        {
+                          "type": "text",
+                          "text": `${row.Note}`,
+                          "wrap": true,
+                          "color": "#FC3F3F", 
+                          "size": "sm",
+                          "flex": 10
                         }
                       ]
                     }
+
                   ]
                 }
-              ]
+              ],
+              // "borderColor": "#002469",
+              // "borderWidth": "3px"
+              "backgroundColor": "#F9F8F7"
             },
-            "footer": {
+            "footer":{
               "type": "box",
               "layout": "vertical",
               "spacing": "sm",
@@ -124,87 +322,65 @@ function handleLocationEvent(event) {
                   "action": {
                     "type": "uri",
                     "label": "นำทาง",
-                    "uri": `https://www.google.com/maps/dir/${event.message.latitude},${event.message.longitude}/${row.geometry.location.lat},${row.geometry.location.lng}`
+                    "uri": `https://www.google.com/maps/dir/${event.message.latitude},${event.message.longitude}/${row.lat},${row.lng}`
                   },
                   "height": "sm",
                   "color": "#012971"
                 },
                 {
                   "type": "button",
-                  "action": {
-                    "type": "uri",
-                    "label": "เว็บไซต์",
-                    "uri": "https://www.uob.co.th/personal/location/locations-line.page"
-                  },
+                  "flex": 2,
+                  "style": "link",
                   "height": "sm",
-                  
-                },
-                {
-                  "type": "spacer",
-                  "size": "sm"
-                }
+                  "action":  {
+                    "type": "uri",
+                    "label": "เปิดบัญชีเงินฝาก",
+                    "uri": "https://uniservices1.uobgroup.com/secure/forms/th/business/business-account-opening/index.html?lang=th?s_cid=default-landing"
+                  }
+                }  
               ],
               "flex": 0
             }
-           
           }))
-
-          var CC = [];
-
-          CC.push(pinData[1])
-
           var msg = {
             "type": "flex",
             "altText": "Flex Message",
             "contents": {
                 "type": "carousel",
-                "contents": CC
-          }
-        }
-  
+                "contents": pinData
+                  }
+                }
           resolve(client.replyMessage(event.replyToken, msg))
         } else {
-          reject()
+          var msg = {
+            "type": "text",
+            "text": "> Missing Data <"                                 
+          }
+          //resolve(client.replyMessage(event.replyToken, msg))
+          reject(msg)
         }
-      })
+
+      } 
+    )
+  
+  }
+
+
+  const postToDialogflow = req => {
+      req.headers.host = "bots.dialogflow.com";
+      return request({
+        method: "POST",
+        uri: 'https://bots.dialogflow.com/line/d5e4e13d-c0cc-4c84-8506-c1d21c965b02/webhook',
+        headers: req.headers,
+        body: JSON.stringify(req.body)
+        });
+    };
+
+
+    app.set('port', (process.env.PORT || 4000))
+    app.use(bodyParser.urlencoded({extended: false}))
+    app.use(bodyParser.json())
+    
+    app.listen(app.get('port'), function () {
+      console.log('run at port', app.get('port'))
     })
-   
-  }
-
-  function calculate(lat1,lon1,lat2,lon2,unit) {
-   // return "sdsd";
-    lat1 = parseFloat(lat1);
-    lat2 = parseFloat(lat2);
-
-    lon1 = parseFloat(lon1);
-    lon2 = parseFloat(lon2);
-
-    if ((lat1 == lat2) && (lon1 == lon2)) {
-      return 0;
-    }
-    else {
-      var radlat1 = Math.PI * lat1/180;
-      var radlat2 = Math.PI * lat2/180;
-      var theta = lon1-lon2;
-      var radtheta = Math.PI * theta/180;
-      var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-      if (dist > 1) {
-        dist = 1;
-      }
-      dist = Math.acos(dist);
-      dist = dist * 180/Math.PI;
-      dist = dist * 60 * 1.1515;
-       if (unit=="K") { dist = dist * 1.609344 }
-       if (unit=="N") { dist = dist * 0.8684 }
-
-       return parseFloat(dist).toFixed(1)+" KM";
-
-    }
-  
-  }
-  app.set('port', (process.env.PORT || 4000))
-  
-  app.listen(app.get('port'), function () {
-    console.log('run at port', app.get('port'))
-  })
-       
